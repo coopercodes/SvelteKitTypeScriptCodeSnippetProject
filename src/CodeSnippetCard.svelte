3dc3472e1963d489b0d24e1e486b580d7ef0760e
Jeison_🧐 <l1996021@gmail.com>
2023-08-26 17:34:48 -0500

    updates for sveltekit version 2023

--- src/CodeSnippetCard.svelte
+++ src/CodeSnippetCard.svelte
@@ -1,31 +1,34 @@
 <script lang="ts">
-    import { CodeBlock } from "@skeletonlabs/skeleton";
+    import { CodeBlock } from '@skeletonlabs/skeleton';
     import { deleteSnippet, toggleFavorite } from "./SnippetStore";
 
-    export let snippet : CodeSnippet = {
+    export let snippet: CodeSnippet = {
         title: "",
         language: "",
         code: "",
         favorite: false
     }
 
-    export let index : number;
+    export let index: number;
+
+    // Destructure the snippet for cleaner access
+    const { title, language, code, favorite } = snippet;
 
 </script>
 
 <div class="card">
     <header class="card-header">
-        {snippet.title}
+        {title}
         <div class="float-right">
-            <button type="button" class="btn btn-sm variant-filled-secondary" on:click={() => toggleFavorite(index)}>
-                { snippet.favorite ? "Unfavorite" : "Favorite" }
+            <button type="button" class="btn btn-sm variant-filled-secondary" on:click={() => toggleFavorite(index)} aria-label={favorite ? "Unfavorite snippet" : "Favorite snippet"}>
+                { favorite ? "Unfavorite" : "Favorite" }
             </button>
-            <button type="button" class="btn btn-sm variant-filled-error" on:click={() => deleteSnippet(index)}>
+            <button type="button" class="btn btn-sm variant-filled-error" on:click={() => deleteSnippet(index)} aria-label="Delete snippet">
                 X
             </button>
         </div>
     </header>
-    <section class="p-4">
-        <CodeBlock language={snippet.language} code={snippet.code}/>
+    <section class="p-3">
+        <CodeBlock language={language} code={code}/>
     </section>
 </div>
